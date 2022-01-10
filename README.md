e56a64a8af84e85de4ce7b55fb4990bacfd2d568
Kyle Fratto <kf9087@gmail.com>
2022-01-09 21:57:45 -0500

    merge test on readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ski-resort-reccomender
 Web app that recommends a ski resort based on a score. The score takes into account: ticket price, current conditions on the mountain, current weather, total snowfall in the past 7 days
+Kyle's test
--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,3 +10,21 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+#Creating the Recommender scale
+
+# Web scraping for trails -- Mtn Creek Specific
+url = "https://www.mountaincreek.com/mountainreport"
+
+content = requests.get(url).text
+# Parse the html content
+soup = BeautifulSoup(content, "html.parser")
+
+open_trails = soup.find("span", {"class": "trail_lift_open"}).text
+print(open_trails)
+
+total_trails = soup.find("span", {"class": "trail_lift_total"}).text
+numeric_filter = filter(str.isdigit, total_trails)
+total_trails = "".join(numeric_filter)
+print(total_trails)
+
+#app.run(host='0.0.0.0', port=81)
