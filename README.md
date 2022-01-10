93421e1b7afb2ac14e88dda65d62189c4a697517
Kyle Fratto <kf9087@gmail.com>
2022-01-10 02:11:38 -0500

    I am able to grab trails data from 2/3 mtns.  Experimenting w selenium for the 3rd mtn not working

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # ski-resort-recommender
 Web app that recommends a ski resort based on a score. The score takes into account: ticket price, current conditions on the mountain, current weather, total snowfall in the past 7 days
-<<<<<<< HEAD
-Kyle's test
-=======
-LINE 3 DAVID
->>>>>>> 08de911bc9067a9a40fcae5897b07d090d9461df
+
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
 
 app = Flask(__name__)
-
+Mtns = {"Mountain Creek": ["https://www.mountaincreek.com/mountainreport",  0, 0], 
+        "Hunter Mountain": ["https://www.huntermtn.com/the-mountain/mountain-conditions/lift-and-terrain-status.aspx", 0, 0],
+        "Killington": ["https://www.killington.com/the-mountain/conditions-weather/current-conditions-weather", 0, 0]
+        }
 
 @app.route('/')
 @app.route('/home')
@@ -13,19 +17,50 @@ def home_page():
 
 #Creating the Recommender scale
 
-# Web scraping for trails -- Mtn Creek Specific
-url = "https://www.mountaincreek.com/mountainreport"
-
-content = requests.get(url).text
+#def refresh_trail_data():
+'''
+# Web scraping for trails -- Mtn Creek Specific -- tested == good
+content = requests.get(Mtns["Mountain Creek"][0]).text
 # Parse the html content
 soup = BeautifulSoup(content, "html.parser")
-
-open_trails = soup.find("span", {"class": "trail_lift_open"}).text
-print(open_trails)
-
+#open trails
+Mtns["Mountain Creek"][1] = soup.find("span", {"class": "trail_lift_open"}).text
+#total trails
 total_trails = soup.find("span", {"class": "trail_lift_total"}).text
 numeric_filter = filter(str.isdigit, total_trails)
-total_trails = "".join(numeric_filter)
-print(total_trails)
+Mtns["Mountain Creek"][2] = "".join(numeric_filter)
+
+
+# Web scraping for trails -- Hunter Mtn Specific -- tested == good
+content = requests.get(Mtns["Hunter Mountain"][0]).text
+soup = BeautifulSoup(content, "html.parser")
+Mtns["Hunter Mountain"][1] = soup.findAll("span", {"class": "c118__number1--v1"})[2].text
+total_trails = soup.findAll("span", {"class": "c118__number2--v1"})[2].text
+numeric_filter = filter(str.isdigit, total_trails)
+Mtns["Hunter Mountain"][2] = "".join(numeric_filter)
+'''
+
+# Web scraping for trails -- Killington Specific -- tested == bad
+DRIVER_PATH = '/path/to/chromedriver'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://google.com')
+
+driver.get(Mtns["Killington"][0])
+print(driver.page_source)
+
+soup = BeautifulSoup(content, "html.parser")
+print(soup.prettify)
+tmp = soup.find("text", {"class": "ng-progress"})
+print(tmp)
+Mtns["Killington"][1] = tmp[0].text
+total_trails = tmp[1].text
+numeric_filter = filter(str.isdigit, total_trails)
+Mtns["Killington"][2] = "".join(numeric_filter)
+
+
+print(Mtns)
+
+
+
 
 #app.run(host='0.0.0.0', port=81)
