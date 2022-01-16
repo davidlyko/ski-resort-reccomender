a9038194dc1499e254dbbd496e1a7e13357cc7dd
Kyle Fratto <kf9087@gmail.com>
2022-01-16 01:16:28 -0500

    program can get trail and lift info about any moutain added to the mountain_names array and stores it

--- app.py
+++ app.py
@@ -10,50 +10,57 @@ import pandas as pd
 import time
 
 app = Flask(__name__)
-Mtns = {"Mountain Creek": ["https://www.mountaincreek.com/mountainreport",  0, 0], 
-        "Hunter Mountain": ["https://www.huntermtn.com/the-mountain/mountain-conditions/lift-and-terrain-status.aspx", 0, 0],
-        "Killington": ["https://www.killington.com/the-mountain/conditions-weather/current-conditions-weather", 0, 0]
-        }
+#Format: { "Moutain X" : [url, lifts open / total lifts, trails open / total trail]}
+Mtns = {}
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
 
-DRIVER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
-ser = Service(DRIVER_PATH)
-op = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=ser, options=op)
-mountain = "Hunter Mountain"
+def create_webdriver(): 
+    DRIVER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+    ser = Service(DRIVER_PATH)
+    op = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=ser, options=op)
+    return driver
 
-#get method to launch the URL
-driver.get("https://www.onthesnow.com/")
-time.sleep(3)
-search_btn = driver.find_element(By.CLASS_NAME, "styles_btnSearch__1DkDs")
-search_btn.click()
-header = driver.find_element(By.CLASS_NAME, "styles_search__35w0k")
-search_bar = header.find_element(By.TAG_NAME, "input")
-search_bar.clear()
-search_bar.send_keys(mountain)
-search_bar.send_keys(Keys.RETURN)
-time.sleep(8)
-header = driver.find_element(By.CLASS_NAME, "styles_link__Ibp28")
-URL = header.get_attribute("href") #first search result link
+# @param string
+def add_mtn(name): 
+    Mtns[name] = ["url", 0, 0]
 
-driver.get(URL) 
-time.sleep(3)
+# @param dictionary
+def update_mtns(mtns): 
+    for mountain in mtns: 
+        driver = create_webdriver()
+        driver.get("https://www.onthesnow.com/")
+        time.sleep(3)
+        search_btn = driver.find_element(By.CLASS_NAME, "styles_btnSearch__1DkDs")
+        search_btn.click()
+        header = driver.find_element(By.CLASS_NAME, "styles_search__35w0k")
+        search_bar = header.find_element(By.TAG_NAME, "input")
+        search_bar.clear()
+        search_bar.send_keys(mountain)
+        search_bar.send_keys(Keys.RETURN)
+        time.sleep(6)
+        header = driver.find_element(By.CLASS_NAME, "styles_link__Ibp28")
+        URL = header.get_attribute("href") #first search result link
+        Mtns[mountain][0] = URL 
+        driver.get(URL) 
+        time.sleep(3)
+        mountain_info = driver.find_elements(By.CLASS_NAME, "styles_value__ocDGV")
+        update_key(mountain, mountain_info)
+        driver.quit()
 
-mountain_info = driver.find_elements(By.CLASS_NAME, "styles_value__ocDGV")
-for mtn in mountain_info: #prints in order: base depth, lifts, trails ---- Different stuff/amounts depending on mtn
-    print(mtn.text)
-
-
-
-
-
-#inputElement.send_keys('1234')
-#inputElement.send_keys(Keys.RETURN)
-
+def update_key(mountain, mountain_info): 
+    #if mtn has extra info
+    if(len(mountain_info) > 3): 
+        Mtns[mountain][1] = mountain_info[2].text
+        Mtns[mountain][2] = mountain_info[3].text
+    else: 
+        Mtns[mountain][1] = mountain_info[1].text
+        Mtns[mountain][2] = mountain_info[2].text
+                
 #driver.quit()
 @app.route('/about')
 def about_page():
@@ -67,35 +74,17 @@ def output_page():
     print(city)
     return render_template('output.html', first_name='Mountain Creek', first_distance='50', first_score='10', second_name='Killington', second_distance='100', second_score='9.5', third_name='Whiteface', third_distance='120', third_score='8.0', fourth_name='Okemo', fourth_distance='200', fourth_score='7.5', fifth_name='Stowe', fifth_distance='350', fifth_score='5.5')
 
+print("Starting program")
+time.sleep(5)
+mountain_names = ["Mountain Creek", "Hunter Mountain"]
+i = 1
+for name in mountain_names: 
+    add_mtn(name)
+    print("Added " + str(i) + " mountain (" + name + ")")
+    i = i + 1
 
-
-
-#Creating the Recommender scale
-
-#def refresh_trail_data():
-'''
-# Web scraping for trails -- Mtn Creek Specific -- tested == good
-content = requests.get(Mtns["Mountain Creek"][0]).text
-# Parse the html content
-soup = BeautifulSoup(content, "html.parser")
-#open trails
-Mtns["Mountain Creek"][1] = soup.find("span", {"class": "trail_lift_open"}).text
-#total trails
-total_trails = soup.find("span", {"class": "trail_lift_total"}).text
-numeric_filter = filter(str.isdigit, total_trails)
-Mtns["Mountain Creek"][2] = "".join(numeric_filter)
-
-
-# Web scraping for trails -- Hunter Mtn Specific -- tested == good
-content = requests.get(Mtns["Hunter Mountain"][0]).text
-soup = BeautifulSoup(content, "html.parser")
-Mtns["Hunter Mountain"][1] = soup.findAll("span", {"class": "c118__number1--v1"})[2].text
-total_trails = soup.findAll("span", {"class": "c118__number2--v1"})[2].text
-numeric_filter = filter(str.isdigit, total_trails)
-Mtns["Hunter Mountain"][2] = "".join(numeric_filter)
-'''
-#print(Mtns)
-
+update_mtns(Mtns)
+print(Mtns)
 
 
 #app.run(host='0.0.0.0', port=81)
