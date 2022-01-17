1ed616d1d3bbd720f0bcf8d21e46633a44b6d17c
davidlyko <dlyko1@binghamton.edu>
2022-01-17 01:05:53 -0500

    linked front end and back end (things to work on: only 3 ski resorts show up, add different parameters to front end and make grading scale

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+from asyncio.windows_events import NULL
 from cgitb import text
 from flask import Flask, render_template, request
 import requests
@@ -15,12 +16,13 @@ import googlemaps
 app = Flask(__name__)
 #Format: { "Moutain X" : [url, lifts open / total lifts, trails open / total trail]}
 Mtns = {}
-api_key = "AIzaSyCvECllOIOI0BNfujzYemh4HoZz8ufe2Lw"
+api_key = ""
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
 
 def create_webdriver(): 
     DRIVER_PATH = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
@@ -83,12 +85,11 @@ def update_key(mountain, mountain_info):
 # @param zip_code = 5 digit int, max_distance = meters @return a list of nearby mountains by name
 def update_mountain_names(zip_code, max_distance):
     gmaps = googlemaps.Client(key=api_key)
-    
     geocode = gmaps.geocode(zip_code)
     lat = geocode[0]['geometry']['location']['lat']
     lng = geocode[0]['geometry']['location']['lng']
     loc = (lat, lng)
-
+    max_distance=float(max_distance)*1609.34
     places = gmaps.places_nearby(location = loc, keyword = "ski resort", radius = max_distance) 
     names = []
     for i in range(len(places["results"])):
@@ -106,14 +107,36 @@ def about_page():
 @app.route('/', methods=['POST'])
 def output_page():
     zipCode = request.form['zip']
-    miles = request.form['miles']
-    print(zip)
-    return render_template('output.html', first_name='Mountain Creek', first_distance='50', first_score='10', second_name='Killington', second_distance='100', second_score='9.5', third_name='Whiteface', third_distance='120', third_score='8.0', fourth_name='Okemo', fourth_distance='200', fourth_score='7.5', fifth_name='Stowe', fifth_distance='350', fifth_score='5.5')
+    distance = request.form['miles']
+    mountain_names = update_mountain_names(zipCode, distance)
+    print(mountain_names)
+    for name in mountain_names: 
+        add_mtn(name)
+    update_mtns()
+    print(Mtns)
+    num_resorts=len(Mtns)
+    
+    if(num_resorts<5):
+        name_one=''
+        second_name=''
+        third_name=''
+        fourth_name=''
+        fifth_name=''
+        names = [name_one, second_name, third_name, fourth_name, fifth_name]
+        for x in range(num_resorts):
+            names[x] = list(Mtns.keys())[x]
+            print(names[x])
+        return render_template('output.html', first_name=names[0], first_distance='50', first_score='10', second_name=names[1], second_distance='100', second_score='9.5', third_name=names[2], third_distance='120', third_score='8.0', fourth_name=names[3], fourth_distance='200', fourth_score='7.5', fifth_name=names[4], fifth_distance='350', fifth_score='5.5')
+    print(zipCode)
+    return render_template('output.html', first_name=list(Mtns.keys())[0], first_distance='50', first_score='10', second_name=list(Mtns.keys())[1], second_distance='100', second_score='9.5', third_name=list(Mtns.keys())[2], third_distance='120', third_score='8.0', fourth_name=list(Mtns.keys())[3], fourth_distance='200', fourth_score='7.5', fifth_name=list(Mtns.keys())[4], fifth_distance='350', fifth_score='5.5')
 
 
 #running main program methods
+'''
 def main(): 
     print("Starting program")
+
+    
 
     mountain_names = update_mountain_names(12442, 30000)
     print(mountain_names)
@@ -123,7 +146,10 @@ def main():
     print(Mtns)
 
 main()
+'''
 
+if __name__ == "__main__":
+    app.run()
 #app.run(host='0.0.0.0', port=81)
 
 
